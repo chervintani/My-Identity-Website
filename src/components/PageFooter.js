7dbe508401fe24577b41ea4e380e5872a3b60b7d
Chervin Tanilon <chervin.tanilon@student.passerellesnumeriques.org>
2021-02-25 18:10:55 +0800

    deploy

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -5,11 +5,7 @@ export default function PageFooter() {
   return (
     <footer id="footer">
       <ul className="copyright">
-        <li>&copy; {config.authorName}</li>
-        <li>
-          Design: <a href="https://unsplash.com/">Unsplash</a> +{' '}
-          <a href="http://html5up.net">HTML5 UP</a>
-        </li>
+        <li>&copy; {config.authorName} | 2021</li>
       </ul>
     </footer>
   );
